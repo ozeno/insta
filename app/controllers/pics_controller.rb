baf8ef07f1d2c0d3d9d159c4cd29dbea971e9402
ozeno <omer.ozen@bil.omu.edu.tr>
2017-08-10 15:54:43 +0300

    Add index

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,5 +1,11 @@
 class PicsController < ApplicationController
-  def index    
+  before_action :set_pic, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pics = Pic.all
+  end
+
+  def show
   end
 
   def new
@@ -8,10 +14,19 @@ class PicsController < ApplicationController
 
   def create
     @pic = Pic.new pic_params
+    if @pic.save
+      redirect_to @pic, notice: 'Saved!'
+    else
+      render 'new'
+    end
   end
 
   private
     def pic_params
       params.require(:pic).permit(:title, :description)
+    end
+
+    def set_pic
+      @pic = Pic.find(params[:id])
     end
 end
