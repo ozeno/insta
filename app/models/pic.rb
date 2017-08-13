8ca81937e58096d2ca958725e5186e488db4d275
ozeno <omer.ozen@bil.omu.edu.tr>
2017-08-13 03:35:36 +0300

    Minor updates and better email validations

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -3,6 +3,7 @@ class Pic < ApplicationRecord
   belongs_to :user
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/, presence: true
+  validates_attachment_presence :image
   validates :title, presence: true, length: { minimum: 5 }
   validates :description, presence: true, length: { minimum: 10 }
 end
