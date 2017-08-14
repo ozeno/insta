6d2ad001484d53812bd4d547ae4952eace575437
ozeno <omer.ozen@bil.omu.edu.tr>
2017-08-14 11:58:01 +0300

    Use .try(:***?) instead of .***?

--- app/helpers/pics_helper.rb
+++ app/helpers/pics_helper.rb
@@ -1,12 +1,12 @@
 module PicsHelper
   def user_allowed?
-    unless @pic.user == current_user || current_user.admin?
+    unless @pic.user == current_user || current_user.try(:admin?)
       flash[:allert] = 'Accsess denied'
       redirect_to @pic
     end
   end
 
   def viuser_allowed?
-    @pic.user == current_user || current_user.admin?
+    @pic.user == current_user || current_user.try(:admin?)
   end
 end
